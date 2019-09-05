60ed763b54a2137884c4c6216150e109c503fbba
sourabh008 <you@example.com>
2019-09-05 23:11:11 +0530

    Updates

---
+++ precache-manifest.9d3a21aa0bd2e73c5ccace1a83eac286.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06223c97342e46091fe559527faa5b93",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "27c63ef6daa32788db42",
+    "url": "/pwa/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b9723ff870a6183c91e1",
+    "url": "/pwa/static/js/2.386b316d.chunk.js"
+  },
+  {
+    "revision": "27c63ef6daa32788db42",
+    "url": "/pwa/static/js/main.b70e6f6f.chunk.js"
+  },
+  {
+    "revision": "d6a81dc63904f3933899",
+    "url": "/pwa/static/js/runtime~main.9f965c69.js"
+  }
+]);
